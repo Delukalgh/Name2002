9e00a9a49c883dad72ba63901e8e604a64093f4b
Deluka Loku Gam Hewage <delukalokugamhewage@Delukas-MacBook-Pro.local>
2023-10-19 11:40:41 -0700

    stuff

---
+++ Declearing Variables/index.js
@@ -0,0 +1,25 @@
+// Delcare 
+
+/** Question 1 
+ * Declare the variable computer 
+ */
+
+var computer; 
+
+/** Question2 
+ * declare the variable pen
+ */
+
+var pen;
+
+/** Question 3
+ * Declare the variable paper bag 
+ */
+
+var paperBag;
+
+/** Question 4
+ * declare the variable other activities 
+ */
+
+var outdoorActivities;
